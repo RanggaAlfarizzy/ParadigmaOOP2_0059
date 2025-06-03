c26d56228ae9619314fc647d302d47b1fa1c47a6
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-03 14:38:49 +0800

    Membuat Class seseorang dan menggunakan fungsi polimorpi yaitu virtual void

---
+++ polimorpi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class Seseorang {
+public:
+    //virtual void pesan() = 0;
+    virtual void pesan(){
+        cout <<"Pesan dari seseorang"<<endl;
+    }
+};
