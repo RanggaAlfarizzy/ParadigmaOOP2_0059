299e2450b4e4e1e3407a3ea99479b1a38ce0d302
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-03 15:08:03 +0800

    mengisi data saklar

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,14 @@ class remoteLampu {
         return saklarNo[i];
     }
 };
+
+int main (){
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "lampu teras rumah");
+    lampuRumah.setSaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setSaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setSaklarNo(3, "lampu dapur");
+
+
+}
